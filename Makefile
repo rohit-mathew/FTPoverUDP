126f572b24e15afdfe0aa6752f8050e4210c6ab3
Rohit Mathew <rohitjmathew@gmail.com>
2012-10-16 12:23:31 -0400

    Code for finding interfaces and updated serverMakefile

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
 PROGS =	server
 
-CC = gcc -g
+CC = gcc 
 
-LIBS =  -lsocket\
-	/home/courses/cse533/Stevens/unpv13e_solaris2.10/libunp.a
+FLAGS = -g -O2
 
-CFLAGS = -I /home/courses/cse533/Stevens/unpv13e_solaris2.10/lib
+LIBS =  -lsocket -lresolv -lnsl -D_REENTRANT \
+	/home/courses/cse533/Stevens/unpv13e_solaris2.10/libunp.a \
+
+CFLAGS = ${FLAGS} -I /home/courses/cse533/Stevens/unpv13e_solaris2.10/lib \
+	-I /home/courses/cse533/Asgn2_code
 
 all: ${PROGS}
 
-server: server.o
-	${CC} ${CFLAGS} server.c -o server ${LIBS}
+server: server.o 
+	${CC} ${CFLAGS} -o $@ /home/courses/cse533/Asgn2_code/get_ifi_info_plus.o server.o ${LIBS}
+
+server.o: server.c
+	${CC} ${CFLAGS} -c server.c
 clean:
 	rm -f ${PROGS} 
 	rm -f *~
--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 /* include udpserv1 */
 #include "unpifi.h"
 
-void	mydg_echo(int, SA *, socklen_t, SA *);
+void	mydg_echo(int);
 
 struct socket_info{
 	int sock_fd;
@@ -13,13 +13,19 @@ struct socket_info{
 int main(int argc, char **argv)
 {
 	printf("\nServer Program \n");
+	int maxfdp1 = 0; 
+	fd_set rset;
+	
 	int sockfd;
+	int i; 
 	const int on = 1;
 	pid_t	child_pid;
-	struct ifi_info		*ifi, *ifihead;
-	struct sockaddr_in	*sa; 
+	struct ifi_info	*ifi, *ifihead;
+	struct sockaddr_in *sa;
+	
 	int server_port;
 	int window_size;
+	
 	struct socket_info intf_info[128];
 	
 	//Read server.in file
@@ -28,7 +34,7 @@ int main(int argc, char **argv)
 	
 	if(fr == NULL)
 	{
-		printf("Unable to open file server.in. Please check if it is in this directory\n");
+		printf("Unable to open file server.in. Please check. \n");
 		exit(0);
 	}
 	
@@ -37,6 +43,7 @@ int main(int argc, char **argv)
 
 	fclose(fr);
 	
+	printf("\nDetails from server.in file :-\n");
 	printf("\nServer Port : %d", server_port);
 	printf("\nWindow Size : %d\n", window_size);
 	
@@ -44,7 +51,7 @@ int main(int argc, char **argv)
 	
 	//Iterating through all the interfaces
 	int noofintf;
-	for (ifihead = ifi = Get_ifi_info(AF_INET, 1);
+	for (ifihead = ifi = Get_ifi_info_plus(AF_INET, 1);
 		 ifi != NULL; ifi = ifi->ifi_next) 
 	{
 
@@ -57,55 +64,82 @@ int main(int argc, char **argv)
 		sa->sin_port = htons(server_port);
 		bind(sockfd, (SA *) sa, sizeof(*sa));
 		printf("\n Bound %s\n", sock_ntop((SA *) sa, sizeof(*sa)));
+		
 		intf_info[noofintf].sock_fd = sockfd;
 		intf_info[noofintf].ipaddress = sa->sin_addr.s_addr;
+		
 		intf_info[noofintf].network_mask = 0;
 		intf_info[noofintf].subnet_address = 0;
+		
 		noofintf = noofintf+1;
 	}
 	noofintf = noofintf-1;
+	/*
 	char str[128];
 	inet_ntop(AF_INET, &(intf_info[1].ipaddress), str, INET_ADDRSTRLEN);
-	printf("Ip address is %s", str);
-	exit(0);
-}
-
-//SPARE CODE
-
-/*
-		if( (child_pid = fork()) != -1)
+	printf("Ip address is %s\n", str);
+	*/
+	
+	for(i=0; i<=noofintf ; i++)
+	{
+		if(intf_info[i].sock_fd > maxfdp1)
+			maxfdp1 = intf_info[i].sock_fd;
+	}
+	
+	while(1)
+	{
+		FD_ZERO(&rset);
+		for(i=0; i<=noofintf ; i++)
 		{
-			if(child_pid == 0)
+			FD_SET(intf_info[i].sock_fd, &rset);
+		}
+		
+		int k = select(maxfdp1, &rset, NULL, NULL, NULL);
+		
+		for(i=0; i<=noofintf ; i++)
+		{
+			if(FD_ISSET(intf_info[i].sock_fd, &rset))
 			{
-				//In Child
-				mydg_echo(sockfd, (SA *) &cliaddr, sizeof(cliaddr), (SA *) sa);
-			}
-			else
-			{
-				//In Server
-				printf("Child (PID: %d) forked for the interface %s", child_pid,
-					sock_ntop(sa, sizeof(sa)));
+				if( (child_pid = fork()) != -1)
+				{
+					if(child_pid == 0)
+					{
+						//In Child
+						mydg_echo(intf_info[i].sock_fd);
+					}
+					else
+					{
+						printf("In the server after the fork");
+						//In Server
+						//printf("Child of(PID: %d) forked for the interface %s", child_pid,
+						//	sock_ntop(sa, sizeof(sa)));
+						
+					}
+				}
 				
 			}
 		}
-		*/
+		
+		
+	}
+	exit(0);
+}
 
-/*
-void
-mydg_echo(int sockfd, SA *pcliaddr, socklen_t clilen, SA *myaddr)
+
+void mydg_echo(int sockfd)
 {
-	int			n;
-	char		mesg[MAXLINE];
-	socklen_t	len;
+	int n;
+	char mesg[MAXLINE];
+	struct sockaddr_in *pcliaddr;
+	socklen_t len = sizeof(pcliaddr);
 
 	for ( ; ; ) {
-		len = clilen;
-		n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-		printf("child %d, datagram from %s", getpid(),
-			   Sock_ntop(pcliaddr, len));
-		printf(", to %s\n", Sock_ntop(myaddr, clilen));
+		n = recvfrom(sockfd, mesg, MAXLINE, 0, (SA*) pcliaddr, &len);
+		//printf("child %d, datagram from %s", getpid(),
+	//		   Sock_ntop(pcliaddr, len));
+		//printf(", to %s\n", Sock_ntop(myaddr, clilen));
 
 		sendto(sockfd, mesg, n, 0, pcliaddr, len);
 	}
 }
-/* end mydg_echo */
+
