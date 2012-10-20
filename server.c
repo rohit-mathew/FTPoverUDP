954efc0284cd34692b53eac54bc7cf6bbb223dd7
Rohit Mathew <rohitjmathew@gmail.com>
2012-10-19 22:22:25 -0400

    Added code for child to close all sockets

--- server.c
+++ server.c
@@ -6,11 +6,45 @@
 struct socket_info{
 	int sockfd;
 	uint32_t ipaddress;
-	uint32_t network_mask;
-	uint32_t subnet_address;
+	uint32_t networkmask;
+	uint32_t subnetaddress;
  	};
+ 	
+struct socket_info intf_info[32];
 
-void	mydg_echo(struct socket_info);
+void mydg_echo(struct socket_info this_socket, struct sockaddr *pcliaddr, socklen_t clilen, int noofintf)
+{
+	printf("\nChild %d: Reached mydg_echo\n", getpid());
+	int n, i = 0;
+	char mesg[MAXLINE];
+	char str[INET_ADDRSTRLEN];
+	struct sockaddr_in myaddr;
+	socklen_t	len;
+	
+	//Code to close all the sockets except this_socket.sockfd
+	printf("\nChild %d: Closing all sockets except current socket: %d\n", getpid(), this_socket.sockfd);
+	for(i=0 ; i<=noofintf ; i++)
+	{
+		if(this_socket.sockfd == intf_info[i].sockfd)
+			continue;
+		printf("\nChild %d: Closing socket: %d\n", getpid(), intf_info[i].sockfd);
+		close(intf_info[i].sockfd);
+	}
+	
+	myaddr.sin_addr.s_addr = this_socket.ipaddress;
+	
+	//inet_ntop(AF_INET, &(myaddr.sin_addr.s_addr), str, INET_ADDRSTRLEN);
+	//printf("\nIP Address is \t%s\n", str);
+	
+	for ( ; ; ) 
+	{
+		len = clilen;
+		n = recvfrom(this_socket.sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+		//printf("child %d, datagram from %s", getpid(), Sock_ntop(pcliaddr, len));
+		//printf(", to %s\n", Sock_ntop(myaddr, len));
+		sendto(this_socket.sockfd, mesg, n, 0, pcliaddr, len);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -24,11 +58,10 @@ int main(int argc, char **argv)
 	pid_t	child_pid;
 	struct ifi_info	*ifi, *ifihead;
 	struct sockaddr_in *sa;
+	struct sockaddr_in cliaddr;
 	
 	int server_port;
 	int window_size;
-	
-	struct socket_info intf_info[128];
 	
 	//Read server.in file
 	//TODO Error check here for wrong values input from user
@@ -76,18 +109,18 @@ int main(int argc, char **argv)
 		intf_info[noofintf].ipaddress = sa->sin_addr.s_addr;
 		
 		sa = (struct sockaddr_in *) ifi->ifi_ntmaddr;
-		intf_info[noofintf].network_mask = sa->sin_addr.s_addr;
+		intf_info[noofintf].networkmask = sa->sin_addr.s_addr;
 		
-		intf_info[noofintf].subnet_address = 0;
+		intf_info[noofintf].subnetaddress = 0;
 		
 		noofintf = noofintf+1;
 	}
 	
-	//Sample code just to print values out
+	//Printing all the values out
 	
 	char str[128];
 
-	printf("\n There are %d interfaces.\n", noofintf);	
+	printf("\nThere are %d interfaces.\n", noofintf);	
 	noofintf = noofintf-1;	
 	
 	for(i=0; i<=noofintf ; i++)
@@ -95,9 +128,9 @@ int main(int argc, char **argv)
 		printf("These are the details for interface no : %d\n", i);
 		inet_ntop(AF_INET, &(intf_info[i].ipaddress), str, INET_ADDRSTRLEN);
 		printf("IP Address is \t%s\n", str);
-		inet_ntop(AF_INET, &(intf_info[i].network_mask), str, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &(intf_info[i].networkmask), str, INET_ADDRSTRLEN);
 		printf("Network Mask is %s\n", str);
-		inet_ntop(AF_INET, &(intf_info[i].subnet_address), str, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &(intf_info[i].subnetaddress), str, INET_ADDRSTRLEN);
 		printf("Subnet Address is is %s\n", str);
 		
 		//Finding the maximum socket no
@@ -106,73 +139,49 @@ int main(int argc, char **argv)
 	}
 	
 	maxfdp1 = maxfdp1 + 1;
-	printf("Max : %d\n", maxfdp1);
 	
 	while(1)
-	{		
+	{	
 		FD_ZERO(&rset);
 		for(i=0; i<=noofintf ; i++)
 		{
 			FD_SET(intf_info[i].sockfd, &rset);
 		}
-		
+		printf("\nServer Parent: Waiting at select \n");
 		result = select(maxfdp1, &rset, NULL, NULL, NULL);
-		printf("Result : %d\n", result);
 		
 		if(result == -1)
 		{
-			printf("\n Something wrong with select. Exitting. \n");
-			exit(0);
+			printf("\nServer Parent: Something wrong with select. Resetting \n");
+			//continue;
 		}
-
-		printf("\n Reached here \n");
+		
 		for(i=0; i<=noofintf ; i++)
 		{
 			if(FD_ISSET(intf_info[i].sockfd, &rset))
 			{
-				
-				if( (child_pid = fork()) != -1)
+				printf("\nServer Parent: Incoming datagram on socket : %d. Forking a child to handle this.\n", intf_info[i].sockfd);
+				child_pid = fork();
+			
+				if(child_pid >= 0) // fork was successful
 				{
-					if(child_pid == 0)
+					if(child_pid == 0) // child process
 					{
-						//In Child
-						//mydg_echo(intf_info[i]);
+					    mydg_echo(intf_info[i], (struct sockaddr *) &cliaddr, sizeof(cliaddr), noofintf);
 					}
-					else
+					else //Parent process TODO Anything to be done here
 					{
-						printf("In the server after the fork");
-						//In Server
-						//printf("Child of(PID: %d) forked for the interface %s", child_pid,
-						//	sock_ntop(sa, sizeof(sa)));
-						
+					    printf("\nServer Parent: Parent Process");
 					}
 				}
-				
+				else // fork failed
+				{
+					printf("\nServer Parent: Forking the child failed. Exitting\n");
+					exit(0);
+				}
 			}
 		}
-		
-		
-	}
-	exit(0);
-}
-
-
-void mydg_echo(struct socket_info recvd_socket_info)
-{
-	int n;
-	char mesg[MAXLINE];
-	struct sockaddr_in *pcliaddr, *myaddr;
-	socklen_t len = sizeof(pcliaddr);
-	
-	myaddr->sin_addr.s_addr = recvd_socket_info.ipaddress;
-
-	for ( ; ; ) 
-	{
-		n = recvfrom(recvd_socket_info.sockfd, mesg, MAXLINE, 0, (SA*) pcliaddr, &len);
-		printf("child %d, datagram from %s", getpid(), Sock_ntop(pcliaddr, len));
-		printf(", to %s\n", Sock_ntop(myaddr, len));
-
-		sendto(recvd_socket_info.sockfd, mesg, n, 0, pcliaddr, len);
 	}
 }
+
 
